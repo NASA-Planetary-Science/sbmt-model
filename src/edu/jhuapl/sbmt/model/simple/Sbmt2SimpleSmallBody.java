68ead4d37fa4c03cf3bd45533459fc13ff2437ee
James Peachey <James.Peachey@jhuapl.edu>
2017-10-31 23:01:18 -0400

    Copy of SimpleSmallBody that expects same layout as sbmt2 branch code.

---
+++ src/edu/jhuapl/sbmt/model/simple/Sbmt2SimpleSmallBody.java
@@ -0,0 +1,32 @@
+package edu.jhuapl.sbmt.model.simple;
+
+import edu.jhuapl.sbmt.client.SmallBodyModel;
+import edu.jhuapl.sbmt.client.SmallBodyViewConfig;
+
+public class Sbmt2SimpleSmallBody extends SmallBodyModel
+{
+    static private String[] getImageMap(SmallBodyViewConfig config)
+    {
+        return new String[] { config.serverPath("basemap/image_map.png") };
+    }
+
+    public Sbmt2SimpleSmallBody(SmallBodyViewConfig config)
+    {
+        super();
+        initializeConfigParameters(
+                config.hasImageMap ? getImageMap(config) : null,
+                false);
+    }
+
+    @Override
+    public double getDensity()
+    {
+        return getSmallBodyConfig().density;
+    }
+
+    @Override
+    public double getRotationRate()
+    {
+        return getSmallBodyConfig().rotationRate;
+    }
+}
