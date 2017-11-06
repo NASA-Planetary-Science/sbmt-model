6e5ada3d8b77942056c48c810b9a3e682508d000
James Peachey <James.Peachey@jhuapl.edu>
2017-11-06 14:02:11 -0500

    Get rid of mask copied from Amica.

--- src/edu/jhuapl/sbmt/model/ryugu/ONCImage.java
+++ src/edu/jhuapl/sbmt/model/ryugu/ONCImage.java
@@ -13,8 +13,6 @@ import edu.jhuapl.saavtk.util.SafePaths;
 import edu.jhuapl.sbmt.client.SmallBodyModel;
 import edu.jhuapl.sbmt.model.image.PerspectiveImage;
 
-import nom.tam.fits.BasicHDU;
-import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
 
 public class ONCImage extends PerspectiveImage
@@ -42,33 +40,7 @@ public class ONCImage extends PerspectiveImage
     @Override
     protected int[] getMaskSizes()
     {
-        // Cribbed the body of this method from AmicaImage at the time ONCImage was created.
-        String filename = getFitFileFullPath();
-
-        try
-        {
-            Fits f = new Fits(filename);
-            BasicHDU h = f.getHDU(0);
-
-            int startH = h.getHeader().getIntValue("START_H");
-            int startV = h.getHeader().getIntValue("START_V");
-            int lastH  = h.getHeader().getIntValue("LAST_H");
-            int lastV  = h.getHeader().getIntValue("LAST_V");
-
-            f.getStream().close();
-
-            return new int[]{startV, 1023-lastH, 1023-lastV, startH};
-        }
-        catch (FitsException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        // Should never reach here
+    	// Don't mask anything.
         return new int[]{0, 0, 0, 0};
     }
 
