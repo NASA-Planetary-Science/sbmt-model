8740030ab74052d739619c0df53d86af16d8c0ea
James Peachey <James.Peachey@jhuapl.edu>
2017-11-06 11:45:15 -0500

    Find sumfiles; look for images in first FITS extension (not primary).

--- src/edu/jhuapl/sbmt/model/ryugu/ONCImage.java
+++ src/edu/jhuapl/sbmt/model/ryugu/ONCImage.java
@@ -1,7 +1,15 @@
 package edu.jhuapl.sbmt.model.ryugu;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 
+import com.google.common.collect.ImmutableMap;
+
+import edu.jhuapl.saavtk.util.FileCache;
+import edu.jhuapl.saavtk.util.SafePaths;
 import edu.jhuapl.sbmt.client.SmallBodyModel;
 import edu.jhuapl.sbmt.model.image.PerspectiveImage;
 
@@ -12,11 +20,23 @@ import nom.tam.fits.FitsException;
 public class ONCImage extends PerspectiveImage
 {
 
+    private static ImmutableMap<String, String> SUMFILE_MAP = null;
+
     public ONCImage(ImageKey key, SmallBodyModel smallBodyModel,
             boolean loadPointingOnly) throws FitsException, IOException
     {
         super(key, smallBodyModel, loadPointingOnly);
         // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void initialize() throws FitsException, IOException
+    {
+        // Note this is a really bad idea in general (overriding something that is called
+        // from the base class constructor), but this kind of thing is done
+        // a lot in SBMT, so for now, just conforming to the pattern.
+        fitFileImageExtension = 1;
+        super.initialize();
     }
 
     @Override
@@ -52,4 +72,66 @@ public class ONCImage extends PerspectiveImage
         return new int[]{0, 0, 0, 0};
     }
 
+    @Override
+    protected String initializeFitFileFullPath()
+    {
+        return FileCache.getFileFromServer(getImageFileName()).getAbsolutePath();
+    }
+
+    @Override
+    protected String initializeInfoFileFullPath()
+    {
+        ImageKey key = getKey();
+        File keyFile = new File(key.name);
+        String sumFilename = keyFile.getParentFile().getParent()
+        + "/infofiles/" + keyFile.getName() + ".INFO";
+        return FileCache.getFileFromServer(sumFilename).getAbsolutePath();
+    }
+
+    @Override
+    protected String initializeSumfileFullPath()
+    {
+        // SUMFILE_MAP = null; // Just for use while debugging.
+        ImmutableMap<String, String> sumfileMap = getSumfileMap();
+        String imageKey = getImageFileName().replaceFirst(".*/", "");
+        String sumfileName = sumfileMap.get(imageKey);
+        if (sumfileName == null) {
+            throw new NullPointerException("Cannot determine correct sumfile for image " + imageKey);
+        }
+        String sumfileDir = new File(getImageFileName()).getParentFile().getParent();
+        return FileCache.getFileFromServer(SafePaths.getString(sumfileDir, "sumfiles", sumfileName)).getAbsolutePath();
+    }
+
+    private String getImageFileName()
+    {
+        return key.name + ".fit";
+    }
+
+    private ImmutableMap<String, String> getSumfileMap()
+    {
+        if (SUMFILE_MAP == null)
+        {
+            ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+            File keyFile = new File(getImageFileName());
+            File mapFile = FileCache.getFileFromServer(SafePaths.getString(keyFile.getParentFile().getParent(), "make_sumfiles.in"));
+            try (BufferedReader br = new BufferedReader(new FileReader(mapFile)))
+            {
+                while (br.ready())
+                {
+                    String[] line = br.readLine().split("\\s\\s*");
+                    if (line.length < 2) throw new ParseException("Cannot parse line " + String.join(" ", line) + " to get sum file/image file names", line.length > 0 ? line[0].length() : 0);
+                    String sumFile = line[0] + ".SUM";
+                    String imageFile = line[line.length - 1].replace("xx", "");
+                    builder.put(imageFile, sumFile);
+                }
+            }
+            catch (IOException | ParseException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            SUMFILE_MAP = builder.build();
+        }
+        return SUMFILE_MAP;
+    }
 }
