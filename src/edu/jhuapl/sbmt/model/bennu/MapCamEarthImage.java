bcdc4ca722e689df711a468e92c04df60d15314e
nguyel1 <nguyel1@NGUYEL1-WL3.dom1.jhuapl.edu>
2017-11-07 09:48:52 -0500

    adding new image classes for OCAMS

---
+++ src/edu/jhuapl/sbmt/model/bennu/MapCamEarthImage.java
@@ -0,0 +1,27 @@
+package edu.jhuapl.sbmt.model.bennu;
+
+import java.io.IOException;
+
+import vtk.vtkImageData;
+
+import edu.jhuapl.sbmt.client.SmallBodyModel;
+
+import nom.tam.fits.FitsException;
+
+public class MapCamEarthImage extends MapCamImage
+{
+    public MapCamEarthImage(ImageKey key,
+            SmallBodyModel smallBodyModel,
+            boolean loadPointingOnly) throws FitsException, IOException
+    {
+        super(key, smallBodyModel, loadPointingOnly);
+    }
+
+    @Override
+    protected void processRawImage(vtkImageData rawImage)
+    {
+        // For some reason we need to do
+        // this so the image is displayed properly.
+//        ImageDataUtil.rotateImage(rawImage, 90);
+    }
+}
---
+++ src/edu/jhuapl/sbmt/model/bennu/SamCamEarthImage.java
@@ -0,0 +1,27 @@
+package edu.jhuapl.sbmt.model.bennu;
+
+import java.io.IOException;
+
+import vtk.vtkImageData;
+
+import edu.jhuapl.sbmt.client.SmallBodyModel;
+
+import nom.tam.fits.FitsException;
+
+public class SamCamEarthImage extends MapCamImage
+{
+    public SamCamEarthImage(ImageKey key,
+            SmallBodyModel smallBodyModel,
+            boolean loadPointingOnly) throws FitsException, IOException
+    {
+        super(key, smallBodyModel, loadPointingOnly);
+    }
+
+    @Override
+    protected void processRawImage(vtkImageData rawImage)
+    {
+        // Flip image along X axis. For some reason we need to do
+        // this so the image is displayed properly.
+//        ImageDataUtil.flipImageXAxis(rawImage);
+    }
+}
