ac0b0689603ed3e97414ecec728f621af4af6589
zimmemi1 <Michael.Zimmerman@jhuapl.edu>
2017-11-12 13:54:32 -0500

    Added mapcamv4 and polycamv4 image classes. Still need to tie them into the application.

---
+++ src/edu/jhuapl/sbmt/model/bennu/MapCamV4Image.java
@@ -0,0 +1,28 @@
+package edu.jhuapl.sbmt.model.bennu;
+
+import java.io.IOException;
+
+import vtk.vtkImageData;
+
+import edu.jhuapl.sbmt.client.SmallBodyModel;
+import edu.jhuapl.sbmt.util.ImageDataUtil;
+
+import nom.tam.fits.FitsException;
+
+public class MapCamV4Image extends MapCamImage
+{
+    public MapCamV4Image(ImageKey key,
+            SmallBodyModel smallBodyModel,
+            boolean loadPointingOnly) throws FitsException, IOException
+    {
+        super(key, smallBodyModel, loadPointingOnly);
+    }
+
+    @Override
+    protected void processRawImage(vtkImageData rawImage)
+    {
+        // Flip image along y axis. For some reason we need to do
+        // this so the image is displayed properly.
+        ImageDataUtil.flipImageYAxis(rawImage);
+    }
+}
