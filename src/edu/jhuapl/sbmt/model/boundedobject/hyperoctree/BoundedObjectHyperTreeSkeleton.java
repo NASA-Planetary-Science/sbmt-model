c863928d5c37b14f78fdeb38a4af881523dfe15b
Colleen <colleen.oshea@jhuapl.edu>
2018-09-04 13:00:55 -0400

    fix more hard-coded dimension things

--- src/edu/jhuapl/sbmt/model/boundedobject/hyperoctree/BoundedObjectHyperTreeSkeleton.java
+++ src/edu/jhuapl/sbmt/model/boundedobject/hyperoctree/BoundedObjectHyperTreeSkeleton.java
@@ -1,7 +1,12 @@
 package edu.jhuapl.sbmt.model.boundedobject.hyperoctree;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+import edu.jhuapl.saavtk.util.FileCache;
+import edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeNode;
 import edu.jhuapl.sbmt.lidar.hyperoctree.FSHyperTreeSkeleton;
 
 
@@ -12,6 +17,29 @@ public class BoundedObjectHyperTreeSkeleton extends FSHyperTreeSkeleton
     {
         super(dataSourcePath);
     }
+
+    @Override
+    public double[] readBoundsFile(Path path)
+    {
+        File f=FileCache.getFileFromServer(path.toString());
+        if (f.exists()) {
+            return FSHyperTreeNode.readBoundsFile(Paths.get(f.getAbsolutePath()), 8);
+        }
+
+        f=FileCache.getFileFromServer(FileCache.FILE_PREFIX+path.toString());
+        if (f.exists()) {
+            return FSHyperTreeNode.readBoundsFile(Paths.get(f.getAbsolutePath()), 8);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void readChildren(Scanner scanner, Node node)   // cf. OlaFSHyperTreeCondenser for code to write the skeleton
+    {
+        readChildren(scanner, node, 8);
+    }
+
 
 
 }
