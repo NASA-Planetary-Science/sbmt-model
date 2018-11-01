b6dea05790f4a1a1f9ac4bb8f1f0c4d235bf2914
Josh Steele <josh.steele@jhuapl.edu>
2018-11-01 13:10:51 -0400

    initial work for PerspectiveImage cleanup

---
+++ src/edu/jhuapl/sbmt/model/image/PerspectiveImageIOHelpers.java
@@ -0,0 +1,11 @@
+package edu.jhuapl.sbmt.model.image;
+
+public class PerspectiveImageIOHelpers
+{
+
+    public PerspectiveImageIOHelpers()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
