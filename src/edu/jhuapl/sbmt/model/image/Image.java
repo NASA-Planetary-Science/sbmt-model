8b5b39ae067e928a1715615f39f9e4654ef48722
Josh Steele <josh.steele@jhuapl.edu>
2018-11-26 13:11:53 -0500

    debugging

--- src/edu/jhuapl/sbmt/model/image/Image.java
+++ src/edu/jhuapl/sbmt/model/image/Image.java
@@ -115,11 +115,13 @@ public abstract class Image extends AbstractModel implements PropertyChangeListe
         public ImageKey(String name, ImageSource source)
         {
             this(name, source, null, null, null, null, 0);
+            System.out.println("Image.ImageKey: called const 1: ");
         }
 
         public ImageKey(String name, ImageSource source, ImagingInstrument instrument)
         {
             this(name, source, null, null, instrument, null, 0);
+            System.out.println("Image.ImageKey: enclosing_method: called const 2");
         }
 
         public ImageKey(String name, ImageSource source, FileType fileType, ImageType imageType, ImagingInstrument instrument, String band, int slice)
@@ -129,6 +131,7 @@ public abstract class Image extends AbstractModel implements PropertyChangeListe
             this.name = name;
             this.source = source;
             this.fileType = fileType;
+            System.out.println("Image.ImageKey: ImageKey: called 3rd const with filetype " + fileType + " for name " + name);
             this.imageType = instrument != null ? instrument.type : imageType;
             this.instrument = instrument;
             this.band = band;
