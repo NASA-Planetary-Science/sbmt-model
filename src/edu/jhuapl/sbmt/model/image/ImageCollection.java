1a0214897fe75869f01c48baf4885fd055ed6236
Josh Steele <josh.steele@jhuapl.edu>
2019-01-03 17:46:12 -0500

    HOTFIX: Fixes problem with loading custom images

--- src/edu/jhuapl/sbmt/model/image/ImageCollection.java
+++ src/edu/jhuapl/sbmt/model/image/ImageCollection.java
@@ -77,26 +77,26 @@ public class ImageCollection extends AbstractModel implements PropertyChangeList
         Stopwatch sw = new Stopwatch();
         sw.start();
         Image image = createImage(key, smallBodyModel);
-        System.out.println("ImageCollection: addImage: created image in " + sw.elapsedMillis() + " ms");
+//        System.out.println("ImageCollection: addImage: created image in " + sw.elapsedMillis() + " ms");
 
         smallBodyModel.addPropertyChangeListener(image);
         image.addPropertyChangeListener(this);
-        System.out.println("ImageCollection: addImage: putting image in imageToActorsMap " + sw.elapsedMillis() + " ms");
+//        System.out.println("ImageCollection: addImage: putting image in imageToActorsMap " + sw.elapsedMillis() + " ms");
 
         imageToActorsMap.put(image, new ArrayList<vtkProp>());
-        System.out.println("ImageCollection: addImage: getting props " + sw.elapsedMillis() + " ms");
+//        System.out.println("ImageCollection: addImage: getting props " + sw.elapsedMillis() + " ms");
 
         List<vtkProp> imagePieces = image.getProps();
-        System.out.println("ImageCollection: addImage: building actor to image map " + sw.elapsedMillis() + " ms");
+//        System.out.println("ImageCollection: addImage: building actor to image map " + sw.elapsedMillis() + " ms");
 
         imageToActorsMap.get(image).addAll(imagePieces);
-        System.out.println("ImageCollection: addImage: building image to actor map " + sw.elapsedMillis() + " ms");
+//        System.out.println("ImageCollection: addImage: building image to actor map " + sw.elapsedMillis() + " ms");
 
         for (vtkProp act : imagePieces)
             actorToImageMap.put(act, image);
-        System.out.println("ImageCollection: addImage: firing listener " + sw.elapsedMillis() + " ms");
+//        System.out.println("ImageCollection: addImage: firing listener " + sw.elapsedMillis() + " ms");
         this.pcs.firePropertyChange(Properties.MODEL_CHANGED, null, null);
-        System.out.println("ImageCollection: addImage: fired listener " + sw.elapsedMillis() + " ms");
+//        System.out.println("ImageCollection: addImage: fired listener " + sw.elapsedMillis() + " ms");
 
     }
 
