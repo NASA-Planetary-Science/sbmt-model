419f97bcb466bf21cf0201e51cf0f4b9230be8f0
lopeznr1 <lopeznr1@jhuapl.edu>
2019-02-12 12:15:42 -0500

    Promoted TrackFileType to own class.

--- src/edu/jhuapl/sbmt/model/lidar/LidarSearchDataCollection.java
+++ src/edu/jhuapl/sbmt/model/lidar/LidarSearchDataCollection.java
@@ -71,55 +71,6 @@ import edu.jhuapl.sbmt.util.gravity.Gravity;
 
 public class LidarSearchDataCollection extends AbstractModel
 {
-    public enum TrackFileType
-    {
-        LIDAR_ONLY("Lidar Point Only (X,Y,Z)"),
-        LIDAR_WITH_INTENSITY("Lidar Point (X,Y,Z) with Intensity (Albedo)"),
-        TIME_WITH_LIDAR("Time, Lidar Point (X,Y,Z)"),
-        TIME_LIDAR_RANGE("Time, Lidar (X,Y,Z), Range"),
-        TIME_LIDAR_ALBEDO("Time, Lidar (X,Y,Z), Albedo"),
-        LIDAR_SC("Lidar (X,Y,Z), S/C (SCx, SCy, SCz)"),
-        TIME_LIDAR_SC("Time, Lidar (X,Y,Z), S/C (SCx, SCy, SCz)"),
-        TIME_LIDAR_SC_ALBEDO("Time, Lidar (X,Y,Z), S/C (SCx, SCy, SCz), Albedo"),
-        BINARY("Binary"),
-        OLA_LEVEL_2("OLA Level 2"),
-        HAYABUSA2_LEVEL_2("Hayabusa2 Level 2"),
-        PLY("PLY");
-
-        String name;
-
-        private TrackFileType(String name)
-        {
-            this.name = name;
-        }
-
-        public String getName()
-        {
-            return name;
-        }
-
-        public static TrackFileType find(String name)
-        {
-            for (TrackFileType type : values())
-            {
-                if (name == type.getName())
-                    return type;
-            }
-            return null;
-        }
-
-        public static String[] names()
-        {
-            String[] titles = new String[values().length];
-            int i=0;
-            for (TrackFileType type : values())
-            {
-                titles[i++] = type.getName();
-            }
-            return titles;
-        }
-    };
-
     private BodyViewConfig polyhedralModelConfig;
     private PolyhedralModel smallBodyModel;
     private vtkPolyData polydata;   // target points
