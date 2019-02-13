7f0a4fbfba6ec5d7580074b5716a6b4ce6be5287
Josh Steele <josh.steele@jhuapl.edu>
2019-02-13 15:31:13 -0500

    Added new classes to help new data formats at Ryugu

---
+++ src/edu/jhuapl/sbmt/model/ryugu/nirs3/atRyugu/NIRS3SpectrumData.java
@@ -0,0 +1,39 @@
+package edu.jhuapl.sbmt.model.ryugu.nirs3.atRyugu;
+
+import java.util.Scanner;
+
+import edu.jhuapl.sbmt.model.ryugu.nirs3.NIRS3;
+import edu.jhuapl.sbmt.util.TimeUtil;
+
+public class NIRS3SpectrumData
+{
+	double et;
+	double[] spectrum;
+
+	public NIRS3SpectrumData(String filename, String line)
+	{
+		spectrum = new double[NIRS3.bandCentersLength];
+		Scanner scanner = new Scanner(line).useDelimiter(",");
+		scanner.next(); // skip the UTC start
+		String midTime = scanner.next(); // get the UTC mid time
+		//craft the time from the filename + the midTime
+		String midTimeComplete = filename.substring(0, 4) + "-" + filename.substring(4,6) + "-" + filename.substring(6,8) + "T" + midTime;
+		System.out.println("NIRS3SpectrumData: NIRS3SpectrumData: midtime is " + midTimeComplete);
+		et = TimeUtil.str2et(midTimeComplete);
+		scanner.next(); // skip the UTC end
+		for (int i = 0; i < NIRS3.bandCentersLength; i++)
+			spectrum[i] = Double.parseDouble(scanner.next());
+		scanner.close();
+	}
+
+	public double[] getSpectrum()
+	{
+		return spectrum;
+	}
+
+	public double getEt()
+	{
+		return et;
+	}
+
+}
