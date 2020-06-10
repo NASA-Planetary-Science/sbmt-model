a21127f12436f84aa18af2edf90465a971b8aef0
Josh Steele <josh.steele@jhuapl.edu>
2020-06-10 10:43:17 -0400

    Updated OVIRS to be able to use new SpectrumPhpQuery style of getting data

--- src/edu/jhuapl/sbmt/model/bennu/spectra/ovirs/OVIRSQuery.java
+++ src/edu/jhuapl/sbmt/model/bennu/spectra/ovirs/OVIRSQuery.java
@@ -1,9 +1,26 @@
 package edu.jhuapl.sbmt.model.bennu.spectra.ovirs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.joda.time.DateTime;
+
+import edu.jhuapl.sbmt.client.SbmtSpectrumModelFactory;
 import edu.jhuapl.sbmt.query.ISearchResultsMetadata;
+import edu.jhuapl.sbmt.query.QueryBase;
 import edu.jhuapl.sbmt.query.SearchMetadata;
-import edu.jhuapl.sbmt.query.fixedlist.FixedListQuery;
+import edu.jhuapl.sbmt.query.database.DatabaseSearchMetadata;
+import edu.jhuapl.sbmt.query.database.SpectraDatabaseSearchMetadata;
+import edu.jhuapl.sbmt.query.database.SpectrumPhpQuery;
 import edu.jhuapl.sbmt.spectrum.model.core.BasicSpectrum;
+import edu.jhuapl.sbmt.spectrum.model.core.SpectrumInstrumentFactory;
+
+import crucible.crust.metadata.api.Metadata;
+import crucible.crust.metadata.api.Version;
+import crucible.crust.metadata.impl.FixedMetadata;
+import crucible.crust.metadata.impl.SettableMetadata;
 
 /**
  * Query definition class for OVIRS
@@ -11,7 +28,7 @@ import edu.jhuapl.sbmt.spectrum.model.core.BasicSpectrum;
  *
  * This must be final because it is a singleton with a clone() method.
  *
- */public final class OVIRSQuery extends FixedListQuery
+ */public final class OVIRSQuery extends SpectrumPhpQuery
 {
     private static OVIRSQuery instance=new OVIRSQuery();
 
@@ -23,7 +40,7 @@ import edu.jhuapl.sbmt.spectrum.model.core.BasicSpectrum;
 
     private OVIRSQuery()
     {
-        super();
+        super("", "", "");
     }
 
     @Override
@@ -35,15 +52,175 @@ import edu.jhuapl.sbmt.spectrum.model.core.BasicSpectrum;
     @Override
     public String getDataPath()
     {
-        return rootPath + "/spectra";
+    	return "/bennu/shared/ovirs/";
+//        return rootPath + "/spectra";
     }
 
     @Override
     public ISearchResultsMetadata<BasicSpectrum> runQuery(SearchMetadata queryMetadata)
     {
-        // TODO Auto-generated method stub
-        return super.runQuery(queryMetadata);
+        FixedMetadata metadata = queryMetadata.getMetadata();
+        double fromIncidence = metadata.get(DatabaseSearchMetadata.FROM_INCIDENCE);
+        double toIncidence = metadata.get(DatabaseSearchMetadata.TO_INCIDENCE);
+        double fromEmission = metadata.get(DatabaseSearchMetadata.FROM_EMISSION);
+        double toEmission = metadata.get(DatabaseSearchMetadata.TO_EMISSION);
+        String searchString = metadata.get(DatabaseSearchMetadata.SEARCH_STRING);
+        double fromPhase = metadata.get(DatabaseSearchMetadata.FROM_PHASE);
+        double toPhase = metadata.get(DatabaseSearchMetadata.TO_PHASE);
+        double startDistance = metadata.get(DatabaseSearchMetadata.FROM_DISTANCE);
+        double stopDistance = metadata.get(DatabaseSearchMetadata.TO_DISTANCE);
+        DateTime startDate = new DateTime(metadata.get(DatabaseSearchMetadata.START_DATE));
+        DateTime stopDate = new DateTime(metadata.get(DatabaseSearchMetadata.STOP_DATE));
+//        List<Integer> polygonTypes = metadata.get(DatabaseSearchMetadata.POLYGON_TYPES);
+        TreeSet<Integer> cubeList = metadata.get(SpectraDatabaseSearchMetadata.CUBE_LIST);
+        String modelName = metadata.get(SpectraDatabaseSearchMetadata.MODEL_NAME);
+        String dataType = metadata.get(SpectraDatabaseSearchMetadata.DATA_TYPE);
+        spectraTableName = "bennu_" + modelName + "_ovirspectra_" + dataType;
+        cubeTableName = "bennu_" + modelName + "_ovirscubes_" + dataType;
+        List<List<String>> results = null;
+
+        double minIncidence = Math.min(fromIncidence, toIncidence);
+        double maxIncidence = Math.max(fromIncidence, toIncidence);
+        double minEmission = Math.min(fromEmission, toEmission);
+        double maxEmission = Math.max(fromEmission, toEmission);
+        double minPhase = Math.min(fromPhase, toPhase);
+        double maxPhase = Math.max(fromPhase, toPhase);
+
+        try
+        {
+        	boolean tableExists = QueryBase.checkForDatabaseTable(spectraTableName);
+            if (!tableExists) throw new RuntimeException("Database table " + spectraTableName + " is not available now;\n Please contact SBMT Support for assistance");
+
+        	if (searchString != null)
+            {
+                HashMap<String, String> args = new HashMap<>();
+                args.put("searchString", searchString);
+                results = doQuery("searchovirs.php", constructUrlArguments(args));
+            }
+        	else
+        	{
+
+	            double minScDistance = Math.min(startDistance, stopDistance);
+	            double maxScDistance = Math.max(startDistance, stopDistance);
+
+	            HashMap<String, String> args = new HashMap<>();
+	            args.put("startDate", String.valueOf(startDate.getMillis()));
+	            args.put("stopDate", String.valueOf(stopDate.getMillis()));
+	            args.put("minScDistance", String.valueOf(minScDistance));
+	            args.put("maxScDistance", String.valueOf(maxScDistance));
+	            args.put("minIncidence", String.valueOf(minIncidence));
+	            args.put("maxIncidence", String.valueOf(maxIncidence));
+	            args.put("minEmission", String.valueOf(minEmission));
+	            args.put("maxEmission", String.valueOf(maxEmission));
+	            args.put("minPhase", String.valueOf(minPhase));
+	            args.put("maxPhase", String.valueOf(maxPhase));
+	            args.put("spectraTableName", spectraTableName);
+	            args.put("cubeTableName", cubeTableName);
+
+	            if (cubeList != null && cubeList.size() > 0)
+	            {
+	                String cubesStr = "";
+	                int size = cubeList.size();
+	                int count = 0;
+	                for (Integer i : cubeList)
+	                {
+	                    cubesStr += "" + i;
+	                    if (count < size-1)
+	                        cubesStr += ",";
+	                    ++count;
+	                }
+	                args.put("cubes", cubesStr);
+	            }
+
+
+
+	            results = doQuery("searchovirs.php", constructUrlArguments(args));
+	            System.out.println("OVIRSQuery: runQuery: number of results " + results.size());
+        	}
+
+        }
+        catch (RuntimeException re)
+        {
+        	throw re;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+
+        }
+
+        OVIRSSearchResult tempResults = new OVIRSSearchResult();
+        try
+        {
+        	for (List<String> res : results)
+        	{
+            	BasicSpectrum spectrum = SbmtSpectrumModelFactory.createSpectrum(res.get(3),
+            								SpectrumInstrumentFactory.getInstrumentForName("OVIRS"));
+            	tempResults.addResult(spectrum);
+        	}
+        }
+        catch (Exception e)
+        {
+       	 	System.out.println("SpectrumStandardSearch: search: " + e.getLocalizedMessage());
+       	 	e.printStackTrace();
+        }
+        return tempResults;
     }
+
+    // Convert the 0th element of the result (the path to the data)
+    // with the full path, but only if the result does not already have
+    // a full path.
+    protected void changeDataPathToFullPath(List<String> result)
+    {
+        String fullPath = result.get(3);
+//        if (!fullPath.startsWif("/"))
+//        {
+            result.set(3, getDataPath() + "" + fullPath);
+//        }
+    }
+
+    class OVIRSSearchResult implements ISearchResultsMetadata<BasicSpectrum>
+    {
+    	List<BasicSpectrum> results = new ArrayList<BasicSpectrum>();
+
+    	public void addResult(BasicSpectrum spectrum)
+    	{
+    		results.add(spectrum);
+    	}
+
+		@Override
+		public FixedMetadata getMetadata()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<BasicSpectrum> getResultlist()
+		{
+			return results;
+		}
+    }
+
+    @Override
+    public Metadata store()
+    {
+        SettableMetadata configMetadata = SettableMetadata.of(Version.of(1, 0));
+        return configMetadata;
+    }
+
+    @Override
+    public void retrieve(Metadata source)
+    {
+
+    }
+
+//    @Override
+//    public ISearchResultsMetadata<BasicSpectrum> runQuery(SearchMetadata queryMetadata)
+//    {
+//        // TODO Auto-generated method stub
+//        return super.runQuery(queryMetadata);
+//    }
 
 //    @Override
 //    public ISearchResultsMetadata<BasicSpectrum> runQuery(SearchMetadata queryMetadata)
