11edaeace4a3d9c3431e08fb9ba2997b770d5c68
Josh Steele <josh.steele@jhuapl.edu>
2024-05-14 13:46:43 -0400

    Addresses #15 updates or adds files for push to github

---
+++ test/edu/jhuapl/sbmt/model/simple/TestSimpleSmallBody.java
@@ -0,0 +1,34 @@
+package edu.jhuapl.sbmt.model.simple;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TestSimpleSmallBody
+{
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception
+	{
+	}
+
+	@Test
+	void testGetDensity()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRotationRate()
+	{
+		fail("Not yet implemented");
+	}
+
+}
